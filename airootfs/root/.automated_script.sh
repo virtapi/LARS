a2343f120f1a7ff820617767b76bd61b9ba62c2d
Tim Meusel <tim@bastelfreak.de>
2017-04-11 18:31:25 +0200

    implement ipv6 support

--- airootfs/root/.automated_script.sh
+++ airootfs/root/.automated_script.sh
@@ -29,10 +29,37 @@ automated_script ()
     fi
 }
 
+get_mac ()
+{
+  awk 'BEGIN {FS="="; RS=" ";} { if ($1 ~ /BOOTIF/) {gsub(/^[^-]+-/, "", $2); gsub(/-/, ":", $2); print tolower($2)}}' /proc/cmdline
+}
+
+get_interface_we_booted_from ()
+{
+  awk '/'"$1"'/ {sub(/:/, "", $2); print $2}' <<< "$(ip -oneline link show up)"
+}
+
+setup_ipv6 ()
+{
+  if [ -n "$IP6ADDR" ] && [ -n "$IP6GW" ]; then
+    mac="$(get_mac)"
+    interface="$(get_interface_we_booted_from "$mac")"
+    ip -6 addr add "$IP6ADDR" dev "$interface"
+    ip -6 route add default via "$IP6GW" dev "$interface"
+  fi
+}
+
 if [[ $(tty) == "/dev/tty1" ]]; then
   cmdline="$(cat /proc/cmdline)"
   # shellcheck disable=SC2163
-  for i in $cmdline; do case "$i" in HASH=*) export "$i";; esac; done
+  for i in $cmdline; do
+    case "$i" in
+      HASH=*) export "$i";;
+      IP6ADDR=*) export "$i";;
+      IP6GW=*) export "$i";;
+    esac;
+  done
+
   if [ -n "$HASH" ]; then
     grep -v "^root" /etc/shadow > /tmp/shadow.tmp
     GECOS="$(awk -F: '/^root/ {print $3":"$4":"$5":"$6":"$7":"$8":"}' /etc/shadow)"
